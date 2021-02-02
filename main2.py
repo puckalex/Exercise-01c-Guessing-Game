b71a4150dd87b4b1eb5bc14169a302f43081e6e2
puckalex <77993843+puckalex@users.noreply.github.com>
2021-02-01 20:47:02 -0500

    completed assignment (hopefully with extra credit)

--- main2.py
+++ main2.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
-import sys
+import sys, random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+number = random.randrange(1,101)
+while(True):
+    guess = input("Guess a whole number from 1 to 100: ")
+    try:
+        guess = int(guess)
+    except:
+        print("That wasn't a number! Try again!")
+        continue
+    if guess > number:
+        print("Oops! Too high, try again!")
+    elif guess < number:
+        print("Oops! Too low, try again!")
+    elif guess == number: 
+        print("Great job! You got it!")
+        break
